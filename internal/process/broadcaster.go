02635aab2f860aacfea63dca737ff03c086fdbf3
adammcgrogan <adam.mcgrogan@dailypay.com>
2026-08-03 11:29:45 +0100

    fix(process): decouple broadcast from slow console subscribers
    Fixes #20

--- internal/process/broadcaster.go
+++ internal/process/broadcaster.go
@@ -10,34 +10,65 @@ import (
 	"sync"
 )
 
+// subscriberQueueSize bounds how many unwritten lines a subscriber can
+// accumulate before it's treated as a laggard and disconnected, so a stuck
+// socket can't stall broadcast delivery to everyone else.
+const subscriberQueueSize = 256
+
+type subscriber struct {
+	conn net.Conn
+	ch   chan string
+}
+
 type broadcaster struct {
 	mu          sync.Mutex
 	logFile     *os.File
 	stdin       io.WriteCloser
-	subscribers map[net.Conn]struct{}
+	subscribers map[net.Conn]*subscriber
 }
 
 func newBroadcaster(logFile *os.File, stdin io.WriteCloser) *broadcaster {
-	return &broadcaster{logFile: logFile, stdin: stdin, subscribers: map[net.Conn]struct{}{}}
+	return &broadcaster{logFile: logFile, stdin: stdin, subscribers: map[net.Conn]*subscriber{}}
 }
 
 func (b *broadcaster) broadcast(line string) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	b.logFile.WriteString(line)
-	for c := range b.subscribers {
-		_, _ = c.Write([]byte(line))
+	for c, sub := range b.subscribers {
+		select {
+		case sub.ch <- line:
+		default:
+			// Subscriber isn't draining fast enough; drop it rather than
+			// block delivery to the log file and every other subscriber.
+			close(sub.ch)
+			delete(b.subscribers, c)
+			c.Close()
+		}
 	}
 }
 
 func (b *broadcaster) addSubscriber(c net.Conn) {
+	sub := &subscriber{conn: c, ch: make(chan string, subscriberQueueSize)}
 	b.mu.Lock()
-	defer b.mu.Unlock()
-	b.subscribers[c] = struct{}{}
+	b.subscribers[c] = sub
+	b.mu.Unlock()
+
+	go func() {
+		for line := range sub.ch {
+			if _, err := c.Write([]byte(line)); err != nil {
+				b.removeSubscriber(c)
+				return
+			}
+		}
+	}()
 }
 
 func (b *broadcaster) removeSubscriber(c net.Conn) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	delete(b.subscribers, c)
+	if sub, ok := b.subscribers[c]; ok {
+		close(sub.ch)
+		delete(b.subscribers, c)
+	}
 }
